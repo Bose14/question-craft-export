a756a7c7481524702cf4b2ddddd2de7bf45e8f30
BOSE A S <98217889+Bose14@users.noreply.github.com>
2025-07-26 01:59:39 +0530

    Update Login.tsx

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -25,7 +25,7 @@ const Login = () => {
     setIsLoading(true);
 
     try {
-      const res = await fetch("http://localhost:3001/api/login", {
+      const res = await fetch("${import.meta.env.VITE_API_URL}/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email, password }),
